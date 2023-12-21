57afe322e5df872d4349e4fb53f4230347b0e871
Saurabh Shahapure <87181771+saurabhs104@users.noreply.github.com>
2023-12-21 18:09:50 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -1,68 +1,82 @@
-import jwt, datetime, os
-import psycopg2
-from flask import Flask, request
+import os, gridfs, pika, json
+from flask import Flask, request, send_file
+from flask_pymongo import PyMongo
+from auth import validate
+from auth_svc import access
+from storage import util
+from bson.objectid import ObjectId
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 server = Flask(__name__)
 
-def get_db_connection():
-    conn = psycopg2.connect(host=os.getenv('DATABASE_HOST'),
-                            database=os.getenv('DATABASE_NAME'),
-                            user=os.getenv('DATABASE_USER'),
-                            password=os.getenv('DATABASE_PASSWORD'),
-                            port=5432)
-    return conn
+mongo_video = PyMongo(server, uri=os.environ.get('MONGODB_VIDEOS_URI'))
 
+mongo_mp3 = PyMongo(server, uri=os.environ.get('MONGODB_MP3S_URI'))
 
-@server.route('/login', methods=['POST'])
+fs_videos = gridfs.GridFS(mongo_video.db)
+fs_mp3s = gridfs.GridFS(mongo_mp3.db)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq", heartbeat=0))
+channel = connection.channel()
+
+@server.route("/login", methods=["POST"])
 def login():
-    auth_table_name = os.getenv('AUTH_TABLE')
-    auth = request.authorization
-    if not auth or not auth.username or not auth.password:
-        return 'Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'}
+    token, err = access.login(request)
 
-    conn = get_db_connection()
-    cur = conn.cursor()
-    query = f"SELECT email, password FROM {auth_table_name} WHERE email = %s"
-    res = cur.execute(query, (auth.username,))
-    
-    if res is None:
-        user_row = cur.fetchone()
-        email = user_row[0]
-        password = user_row[1]
-
-        if auth.username != email or auth.password != password:
-            return 'Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'}
-        else:
-            return CreateJWT(auth.username, os.environ['JWT_SECRET'], True)
+    if not err:
+        return token
     else:
-        return 'Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'}
+        return err
 
-def CreateJWT(username, secret, authz):
-    return jwt.encode(
-        {
-            "username": username,
-            "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=1),
-            "iat": datetime.datetime.now(tz=datetime.timezone.utc),
-            "admin": authz,
-        },
-        secret,
-        algorithm="HS256",
-    )
+@server.route("/upload", methods=["POST"])
+def upload():
+    access, err = validate.token(request)
 
-@server.route('/validate', methods=['POST'])
-def validate():
-    encoded_jwt = request.headers['Authorization']
-    
-    if not encoded_jwt:
-        return 'Unauthorized', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'}
+    if err:
+        unauth_count.inc()
+        return err
 
-    encoded_jwt = encoded_jwt.split(' ')[1]
-    try:
-        decoded_jwt = jwt.decode(encoded_jwt, os.environ['JWT_SECRET'], algorithms=["HS256"])
-    except:
-        return 'Unauthorized', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'}
-    
-    return decoded_jwt, 200
+    access = json.loads(access)
 
-if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=5000)
+    if access["admin"]:
+        if len(request.files) > 1 or len(request.files) < 1:
+            return "exactly 1 file required", 400
+
+        for _, f in request.files.items():
+            err = util.upload(f, fs_videos, channel, access)
+
+            if err:
+                return err
+
+        return "success!", 200
+    else:
+        return "not authorized", 401
+
+@server.route("/download", methods=["GET"])
+def download():
+    access, err = validate.token(request)
+
+    if err:
+        unauth_count.inc()
+        return err
+
+    access = json.loads(access)
+
+    if access["admin"]:
+        fid_string = request.args.get("fid")
+
+        if not fid_string:
+            return "fid is required", 400
+
+        try:
+            out = fs_mp3s.get(ObjectId(fid_string))
+            return send_file(out, download_name=f"{fid_string}.mp3")
+        except Exception as err:
+            print(err)
+            return "internal server error", 500
+
+    return "not authorized", 401
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=8080)
